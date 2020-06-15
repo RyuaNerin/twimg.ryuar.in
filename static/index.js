3232fbb8008be57e4534de25b4a441a38c0bc9d6
RyuaNerin <admin@ryuar.in>
2020-06-16 05:05:00 +0900

    tsc

--- static/index.js
+++ static/index.js
@@ -156,7 +156,7 @@ $(document).ready(() => {
             if (r.status == 200) {
                 switch (r.responseText) {
                     case "WITH TWIMG-DNS":
-                        $("#with-option").removeAttr("hidden");
+                        $("#with-option").removeClass("invisible");
                         break;
                 }
             }
